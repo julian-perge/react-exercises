e520a6907e653e0d41b6f020baa803b3ca422265
julian-perge <julianperge@gmail.com>
2019-11-06 16:35:44 -0500

    Added a few comments. Added more section elements, and moved indicators inside keypad div. Removed HTML chaining as it was unncessary.

--- use_reducer-keypad/src/Components/Keypad.jsx
+++ use_reducer-keypad/src/Components/Keypad.jsx
@@ -5,28 +5,20 @@ import CorrectPresses from './CorrectPresses';
 import PadButton from './PadButton';
 
 function correctSequence(state, action) {
-	switch (action.isCorrect) {
+	switch (action) {
 	case true:
-		action.parentIndicators.children[2].children[state.correctPresses].classList.add(
-			'correct-indicator'
-		);
-		// document.querySelector(`.keypad-indicators:nth-child(${state.correctPresses + 1})`).classList.add('correct-indicator');
-		// console.log(document.querySelector(`.keypad-indicators:nth-child(${state.correctPresses + 1})`));
 		return {
 			validSequence: state.validSequence,
 			correctPresses: (state.correctPresses += 1)
 		};
 	default:
-		console.log(action.parentIndicators.children[2]);
-		action.parentIndicators.children[2].childNodes.forEach((element) => {
-			element.classList.remove('correct-indicator');
-		});
 		return { validSequence: state.validSequence, correctPresses: 0 };
 	}
 }
 
 function init(keypad) {
 	const arrValidSequence = [];
+	// if a number type, use remainder to get individual digits and push to array
 	if (typeof keypad.validSequence === 'number') {
 		let num = keypad.validSequence;
 		let tmp = 0;
@@ -38,14 +30,17 @@ function init(keypad) {
 		}
 		keypad.validSequence = arrValidSequence;
 	}
+	// keypad.validSequence should either be an array or not false
 	if (!Array.isArray(keypad.validSequence) || !keypad.validSequence) {
 		let counter = 0;
 		while (counter < 6) {
+			// generate digits from 0-9
 			arrValidSequence.push(Math.floor(Math.random() * (9 - 0)) + 1);
 			counter += 1;
 		}
 		keypad.validSequence = arrValidSequence;
 	}
+	// return an Object
 	return keypad;
 }
 
@@ -57,35 +52,40 @@ export default function Keypad({ numberSequence }) {
 	);
 
 	function padButtonPressed(e) {
+		// parse the button value as value returns a String
 		if (
 			Number.parseInt(e.currentTarget.value)
 			=== keypad.validSequence[keypad.correctPresses]
 		) {
-			return dispatch({ isCorrect: true, parentIndicators: e.currentTarget.parentElement.previousElementSibling });
+			return dispatch(true);
 		}
-		return dispatch({
-			isCorrect: false,
-			parentIndicators: e.currentTarget.parentElement.previousElementSibling
-		});
+		return dispatch(false);
 	}
 
 	return (
 		<section className="section-keypad">
 			<header id="keypadDetails">
-				<h1 id="correctSequence">{`The correct sequence of numbers is: ${keypad.validSequence}`}</h1>
-				<CorrectPresses correctPresses={keypad.correctPresses} totalPresses={keypad.validSequence.length} />
+				<h1 className="correct-sequence">{`The correct sequence of numbers is: ${keypad.validSequence}`}</h1>
 			</header>
 			<div className="keypad">
-				<PadButton onAction={padButtonPressed} padNumber={1} />
-				<PadButton onAction={padButtonPressed} padNumber={2} />
-				<PadButton onAction={padButtonPressed} padNumber={3} />
-				<PadButton onAction={padButtonPressed} padNumber={4} />
-				<PadButton onAction={padButtonPressed} padNumber={5} />
-				<PadButton onAction={padButtonPressed} padNumber={6} />
-				<PadButton onAction={padButtonPressed} padNumber={7} />
-				<PadButton onAction={padButtonPressed} padNumber={8} />
-				<PadButton onAction={padButtonPressed} padNumber={9} />
-				<PadButton onAction={padButtonPressed} padNumber={0} />
+				<section className="section-presses">
+					<CorrectPresses
+						correctPresses={keypad.correctPresses}
+						totalPresses={keypad.validSequence.length}
+					/>
+				</section>
+				<section className="section-pad-buttons">
+					<PadButton onAction={padButtonPressed} padNumber={1} />
+					<PadButton onAction={padButtonPressed} padNumber={2} />
+					<PadButton onAction={padButtonPressed} padNumber={3} />
+					<PadButton onAction={padButtonPressed} padNumber={4} />
+					<PadButton onAction={padButtonPressed} padNumber={5} />
+					<PadButton onAction={padButtonPressed} padNumber={6} />
+					<PadButton onAction={padButtonPressed} padNumber={7} />
+					<PadButton onAction={padButtonPressed} padNumber={8} />
+					<PadButton onAction={padButtonPressed} padNumber={9} />
+					<PadButton onAction={padButtonPressed} padNumber={0} />
+				</section>
 			</div>
 		</section>
 	);

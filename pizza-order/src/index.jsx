c71c8f230e20a0c04d1f9639dfa0ab9a5b11e2e2
julian-perge <julianperge@gmail.com>
2019-11-06 21:59:16 -0500

    Added orders.json

--- pizza-order/src/index.jsx
+++ pizza-order/src/index.jsx
@@ -4,10 +4,11 @@ import ReactDOM from 'react-dom';
 import OrderMenu from './Components/OrderMenu';
 
 import './CSS/minireset.min.css';
-import listOfToppings from './Data/listOfToppings.json';
 
+import listOfToppings from './Data/listOfToppings.json';
+import listOfOrders from './Data/listOfPizzaOrders.json'
 
 ReactDOM.render(
-	<OrderMenu listOfToppings={listOfToppings} />,
+	<OrderMenu listOfToppings={listOfToppings} ordersJSON={listOfOrders}/>,
 	document.getElementById('root')
 );
